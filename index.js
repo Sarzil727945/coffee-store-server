80ff83b9801e0026cd1f032c0601ff0755d29512
Sarzil727945 <sarzilmuntaha.gmail.com>
2023-05-09 11:56:02 +0600

    data get

--- index.js
+++ index.js
@@ -25,6 +25,27 @@ async function run() {
      try {
           // Connect the client to the server	(optional starting in v4.7)
           await client.connect();
+
+          const coffeeCollection = client.db("coffeeDB").collection('coffee');
+          
+           //     server to data get start
+           app.get('/coffee', async (req, res) => {
+               const cursor = coffeeCollection.find();
+               const result = await cursor.toArray();
+               res.send(result)
+          })
+          //     server to data get end 
+
+          //     data server post start 
+          app.post('/coffee', async (req, res) => {
+               const newCoffee = req.body;
+               console.log(newCoffee);
+               const result = await coffeeCollection.insertOne(newCoffee);
+               res.send(result)
+          })
+          //     data server post end
+
+
           // Send a ping to confirm a successful connection
           await client.db("admin").command({ ping: 1 });
           console.log("Pinged your deployment. You successfully connected to MongoDB!");
